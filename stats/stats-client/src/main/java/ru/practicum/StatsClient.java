7f1f329f5b7087b5de811a21244cd913135a0924
Novikov Aleksei <3900676@gmail.com>
2026-05-23 02:59:43 +0300

    fix service & infra

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,36 +1,58 @@
 package ru.practicum;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.dto.EndPointHitDto;
 import ru.practicum.dto.StatsParamDto;
 import ru.practicum.dto.ViewStatsDto;
+import ru.practicum.exception.StatsServerUnavailable;
 
+import java.net.URI;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
 public class StatsClient {
 
+    private final DiscoveryClient discoveryClient;
     private final RestTemplate restTemplate;
+    private final RetryTemplate retryTemplate;
+    private final String statsServiceId;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Autowired
-    public StatsClient(@Value("${stats-server.url:http://stats-server:9090}") String serverUrl, RestTemplateBuilder builder) {
-        this.restTemplate = builder
-                .rootUri(serverUrl)
-                .build();
+    public StatsClient(DiscoveryClient discoveryClient, RestTemplateBuilder builder) {
+        this.discoveryClient = discoveryClient;
+        this.statsServiceId = "stats-server";
+        this.restTemplate = builder.build();
+
+        RetryTemplate template = new RetryTemplate();
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(3000L);
+        template.setBackOffPolicy(backOffPolicy);
+
+        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
+        retryPolicy.setMaxAttempts(3);
+        template.setRetryPolicy(retryPolicy);
+
+        this.retryTemplate = template;
     }
 
     public void saveHit(EndPointHitDto hitDto) {
-        restTemplate.postForEntity("/hit", hitDto, Void.class);
+        URI uri = makeUri("/hit");
+        restTemplate.postForEntity(uri, hitDto, Void.class);
     }
 
     public List<ViewStatsDto> getStats(StatsParamDto paramDto) {
@@ -44,13 +66,31 @@ public class StatsClient {
 
         builder.queryParam("unique", paramDto.isUniques());
 
+        URI uri = makeUri(builder.build().toUriString());
+
         ResponseEntity<List<ViewStatsDto>> response = restTemplate.exchange(
-                builder.build().toUriString(),
+                uri,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ViewStatsDto>>() {}
         );
 
         return response.getBody();
+    }
+
+    private URI makeUri(String path) {
+        ServiceInstance instance = retryTemplate.execute(cxt -> getInstance());
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort() + path);
+    }
+
+    private ServiceInstance getInstance() {
+        try {
+            return discoveryClient.getInstances(statsServiceId).getFirst();
+        } catch (Exception exception) {
+            throw new StatsServerUnavailable(
+                    "Ошибка обнаружения адреса сервиса статистики с id: " + statsServiceId,
+                    exception
+            );
+        }
     }
 }
---
+++ stats/stats-client/src/main/java/ru/practicum/exception/StatsServerUnavailable.java
@@ -0,0 +1,8 @@
+package ru.practicum.exception;
+
+public class StatsServerUnavailable extends RuntimeException {
+
+    public StatsServerUnavailable(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
